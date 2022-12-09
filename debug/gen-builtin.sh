fc9c27ffd0e275a5ab11c267e0e4de0b48d60c9a
John-Steve-C <leonardchen701@outlook.com>
2022-12-09 23:08:52 +0800

    builder_almost_complete

--- debug/gen-builtin.sh
+++
@@ -1,2 +0,0 @@
-clang++ builtin.cpp -S -emit-llvm
-# 要自己更改生成.ll文件中的函数名
--- debug/tmp.java
+++
@@ -1,6 +0,0 @@
-import java.io.PrintStream;
-public class tmp {
-	public static void main(String[] args) throws Exception {
-		System.out.println("2333");
-	}
-}
